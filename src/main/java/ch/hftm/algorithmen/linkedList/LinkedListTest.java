df5eeedbc2f3453ab7aeb6410c990b2e72ad1c69
Roman Steger <roman.steger11@gmail.com>
2025-01-18 15:38:21 +0100

    Simple Linked List, alle Methoden eingefügt und Aufgabe 3 erledigt (finde das N-th objekt von hinten

--- src/main/java/ch/hftm/algorithmen/linkedList/LinkedListTest.java
+++ src/main/java/ch/hftm/algorithmen/linkedList/LinkedListTest.java
@@ -5,6 +5,8 @@ public class LinkedListTest {
     // SimpleLinkedList
     System.out.println("Simple Linked List");
     simpleTest1();
+    simpleTest2();
+    simpleTest3();
 
     // LinkedList
     System.out.println("\nLinked List");
@@ -14,15 +16,39 @@ public class LinkedListTest {
 
   public static void simpleTest1(){
     SimpleLinkedListGeneric<Integer> list = new SimpleLinkedListGeneric<>();
-    list.add(10);
-    list.add(20);
-    list.add(30);
+    list.addLast(10);
+    list.addLast(20);
+    list.addLast(30);
     System.out.println(list.toString());
   
     list.reverse();
     System.out.println(list.toString());
   }
 
+  public static void simpleTest2(){
+    SimpleLinkedListGeneric<Integer> list1 = new SimpleLinkedListGeneric<>();
+    list1.addLast(1);
+    list1.addLast(3);
+    list1.addLast(5);
+
+    SimpleLinkedListGeneric<Integer> list2 = new SimpleLinkedListGeneric<>();
+    list2.addLast(2);
+    list2.addLast(4);
+    list2.addLast(6);
+
+    list1.merge(list2);
+    System.out.println(list1.toString());
+  }
+
+  public static void simpleTest3(){
+    SimpleLinkedListGeneric<Integer> list = new SimpleLinkedListGeneric<>();
+    list.addLast(10);
+    list.addLast(20);
+    list.addLast(30);
+    list.addLast(40);
+    list.addLast(50);
+    System.out.println(list.findNthFromEnd(2));
+  }
   
   public static void test1(){
     LinkedListGeneric<Integer> list = new LinkedListGeneric<>();
--- src/main/java/ch/hftm/algorithmen/linkedList/SimpleLinkedListGeneric.java
+++ src/main/java/ch/hftm/algorithmen/linkedList/SimpleLinkedListGeneric.java
@@ -9,7 +9,7 @@ public class SimpleLinkedListGeneric<T> {
     size = 0;
   }
 
-  public void add(T object){
+  public void addLast(T object){
     SimpleNode<T> node = getLastNode();
     SimpleNode<T> newNode = new SimpleNode<>(object, null);
     if(node == null){
@@ -18,6 +18,69 @@ public class SimpleLinkedListGeneric<T> {
       node.setNextNode(newNode);
     }
     size++;
+  }
+  
+  public void addFirst(T object){
+    SimpleNode<T> newNode = new SimpleNode<>(object, rootNode);
+    rootNode = newNode;
+    size++;
+  }
+
+  public boolean contains(T object){
+    return getIndex(object) >= 0 ? true : false;
+  }
+
+  private int getIndex(T object){
+    SimpleNode<T> node = rootNode;
+    int i = 0;
+    while(node != null){
+      if(node.getObject() == object){
+        return i;
+      }
+      node = node.getNextNode();
+      i++;
+    }
+    return -1;
+  }
+
+  private T remove(int index){
+    return removeInternal(get(index));
+  }
+
+  public boolean remove(T object){
+    return removeInternal(object) == null ? false : true;
+  }
+
+  private T removeInternal(T object){
+    SimpleNode<T> node = rootNode;
+    SimpleNode<T> previusNode = null;
+    while(node != null){
+      if(node.getObject() == object){
+        if(previusNode == null){
+          rootNode = node.getNextNode();
+        } else{
+          previusNode.setNextNode(node.getNextNode());
+        }
+        size--;
+        return node.getObject();
+      }
+      previusNode = node;
+      node = node.getNextNode();
+    }
+    return null;
+  }
+
+  public T removeFirst(){
+    SimpleNode<T> node = rootNode;
+    if(rootNode != null){
+      rootNode = rootNode.getNextNode();
+      size--;
+    }
+    return node.getObject();
+  }
+
+  public T removeLast(){
+    return removeInternal(getLast());
   }
 
   public void reverse(){
@@ -33,30 +96,28 @@ public class SimpleLinkedListGeneric<T> {
     rootNode = previusNode;
   }
 
-  // public void merge(LinkedListGeneric<T> list2){
-  //   SimpleNode<T> firstNode1 = rootNode;
-  //   SimpleNode<T> firstNode2 = list2.getFirstNode();
-  //   LinkedListGeneric<T> newList = new LinkedListGeneric<>();
-  //   while(firstNode1 != null || firstNode2 != null){
-  //     if(firstNode1 != null){
-  //       newList.add(firstNode1.getObject());
-  //       firstNode1 = firstNode1.getNextNode();
-  //     }
-  //     if(firstNode2 != null){
-  //       newList.add(firstNode2.getObject());
-  //       firstNode2 = firstNode2.getNextNode();
-  //     }
-  //   }
-  //   rootNode = newList.getFirstNode();
-  //   lastNode = newList.getLastNode();
-  //   size = newList.size;
-  // }
-
-
+  public void merge(SimpleLinkedListGeneric<T> list2){
+    SimpleNode<T> firstNode1 = rootNode;
+    SimpleNode<T> firstNode2 = list2.getFirstNode();
+    SimpleLinkedListGeneric<T> newList = new SimpleLinkedListGeneric<>();
+    while(firstNode1 != null || firstNode2 != null){
+      if(firstNode1 != null){
+        newList.addLast(firstNode1.getObject());
+        firstNode1 = firstNode1.getNextNode();
+      }
+      if(firstNode2 != null){
+        newList.addLast(firstNode2.getObject());
+        firstNode2 = firstNode2.getNextNode();
+      }
+    }
+    rootNode = newList.getFirstNode();
+    size = newList.size;
+  }
+  
   private SimpleNode<T> getFirstNode(){
     return rootNode;
   }
-
+  
   private SimpleNode<T> getLastNode(){
     SimpleNode<T> actNode = rootNode;
     if(rootNode != null){
@@ -66,18 +127,38 @@ public class SimpleLinkedListGeneric<T> {
     }
     return actNode;
   }
-
+  
   public T getFirst(){
     return rootNode.getObject();
   }
+  
+  public T findNthFromEnd(int n){
+    return get(size - n);
+  }
+
+  public T get(int index){
+    SimpleNode<T> node = rootNode;
+    int i = index;
+    while(i > 0){
+      if(node != null){
+        node = node.getNextNode();
+        i--;
+      } else{
+        break;
+      }
+    }
+    if(node == null){
+      return null;
+    }
+    return node.getObject();
+  }
 
   public T getLast(){
-    T object = null;
     SimpleNode<T> node = getLastNode();
     if(node != null){
-      object = node.getObject();
+      return node.getObject();
     }
-    return object;
+    return null;
   }
 
   public boolean isEmpty(){
